dc4f701b73657e314d5420519326077b56b44f14
jejze4364 <63626052+jejze4364@users.noreply.github.com>
2025-06-02 10:38:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -668,19 +668,19 @@ function showResults() {
   // Determinar a página de promoções apropriada baseada no município selecionado
   const promoPageURL = municipioPromoPages[selectedMunicipio] || "promocoes.html";
 
-  container.innerHTML = `
-    <div class="action-buttons">
-      <a href="index.html" class="btn-primary">
-        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-width="2"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/><polyline points="9 22 9 12 15 12 15 22"/></svg>
-        Conheça o Projeto
-      </a>
-      <a href="${promoPageURL}" class="btn-promo">
-        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="none" stroke="currentColor" stroke-width="2">
-          <polygon points="13 2 3 14 12 14 11 22 21 10 12 10 13 2"/>
-        </svg>
-        Conheça as Promoções <span class="btn-indicator">→</span>
-      </a>
-    </div>
+container.innerHTML = `
+  <div class="action-buttons">
+    <a href="index.html" class="btn-primary">
+      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-width="2"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/><polyline points="9 22 9 12 15 12 15 22"/></svg>
+      Conheça o Projeto
+    </a>
+    <a href="promocoes.html" class="btn-promo">
+      <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="none" stroke="currentColor" stroke-width="2">
+        <polygon points="13 2 3 14 12 14 11 22 21 10 12 10 13 2"/>
+      </svg>
+      Conheça as Promoções <span class="btn-indicator">→</span>
+    </a>
+  </div>
 
     <div class="results-container">
       <div class="results-header">
